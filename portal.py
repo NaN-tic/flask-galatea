71bc4893e02b08e31e3d281cb0247c4d366d275e
Raimon Esteve <raimonesteve@gmail.com>
2021-05-19 10:52:10 +0200

    Upper default country when is not null, try/except mail and default language in registration
    #042262

--- portal.py
+++ portal.py
@@ -35,6 +35,7 @@ REGISTRATION_VAT = current_app.config.get('REGISTRATION_VAT')
 REGISTRATION_VAT_CHECK_CUSTOMER = current_app.config.get(
     'REGISTRATION_VAT_CHECK_CUSTOMER', False)
 DEFAULT_COUNTRY = current_app.config.get('DEFAULT_COUNTRY')
+DEFAULT_LANGUAGE = current_app.config.get('LANGUAGE')
 REDIRECT_AFTER_LOGIN = current_app.config.get('REDIRECT_AFTER_LOGIN')
 REDIRECT_AFTER_LOGOUT = current_app.config.get('REDIRECT_AFTER_LOGOUT')
 LOGIN_REMEMBER_ME = current_app.config.get('LOGIN_REMEMBER_ME', False)
@@ -148,6 +149,10 @@ class RegistrationForm(Form):
             if getattr(self, field).type == 'SelectField':
                 if not getattr(self, field).choices and not getattr(self, field).flags.required:
                     delattr(self, field)
+        if not self.vat_country.data:
+            self.vat_country.data = ''
+        if not self.language.data:
+            self.language.data = g.language or DEFAULT_LANGUAGE
         rv = Form.validate(self)
         if not rv:
             return False
@@ -377,7 +382,11 @@ def send_activation_email(user):
             html = render_template('emails/activation-html.jinja', user=user),
             sender = current_app.config.get('DEFAULT_MAIL_SENDER'),
             recipients = [user.email])
-    mail.send(msg)
+    try:
+        mail.send(msg)
+    except ConnectionRefusedError:
+        current_app.logger.error('Error send email!')
+        abort(500)
 
 def send_new_password(user):
     """
@@ -735,7 +744,7 @@ def registration(lang):
                 [m for em in form.errors.values() for m in em])
             flash(error_messages, 'danger')
 
-    form.vat_country.data = DEFAULT_COUNTRY.upper() or ''
+    form.vat_country.data = DEFAULT_COUNTRY and DEFAULT_COUNTRY.upper() or ''
     return render_template('registration.html', form=form, website=website)
 
 @portal.route('/subdivisions', methods=['GET'], endpoint="subdivisions")
