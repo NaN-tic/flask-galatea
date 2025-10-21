18a3c7b83c2fdcfdc58a116966484a6c998f1fb0
Raimon Esteve <raimonesteve@gmail.com>
2025-10-21 16:15:42 +0200

    Remove cache uncomment

--- helpers.py
+++ helpers.py
@@ -4,8 +4,6 @@
 from flask import redirect, url_for, session,  request, current_app, abort
 from functools import wraps
 
-# cache = getattr(current_app, 'cache', None)
-
 def secure(function):
     @wraps(function)
     def decorated_function(*args, **kwargs):
@@ -32,29 +30,6 @@ def manager_required(f):
             abort(404)
         return f(*args, **kwargs)
     return decorated_function
-
-# if cache:
-#     def cached(timeout=5 * 60, key='view/%s'):
-#         def decorator(f):
-#             @wraps(f)
-#             def decorated_function(*args, **kwargs):
-#                 cache_key = key
-#                 rv = cache.get(cache_key)
-#                 if rv is not None:
-#                     return rv
-#                 rv = f(*args, **kwargs)
-#                 cache.set(cache_key, rv, timeout=timeout)
-#                 return rv
-#             return decorated_function
-#         return decorator
-# else:
-#     def cached(timeout=5 * 60, key='view/%s'):
-#         def decorator(f):
-#             @wraps(f)
-#             def decorated_function(*args, **kwargs):
-#                 return f(*args, **kwargs)
-#             return decorated_function
-#         return decorator
 
 def get_cache():
     """Retorna la instància de cache configurada a current_app, si existeix."""
