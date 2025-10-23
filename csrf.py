bcb08f978ed907fd5b0a406286285d7c1f11da36
Raimon Esteve <raimonesteve@gmail.com>
2025-10-23 15:15:19 +0200

    Flask3
    #178864

--- csrf.py
+++
@@ -1,9 +0,0 @@
-#This file is part galatea blueprint for Flask.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-from flask import current_app
-from flask_wtf.csrf import CSRFProtect
-
-with current_app.app_context():
-    csrf = CSRFProtect()
-    csrf.init_app(current_app)
